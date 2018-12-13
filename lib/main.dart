fe833c394cd6f166790c990ea17f690bdfdccbf2
Alejandro Paredes <manuel.paredes@dsindigo.com>
2018-12-12 21:01:37 -0600

    Solicita por medio de internet datos e imprime en pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:random_string/random_string.dart' as random;
 
 void main() => runApp(MyApp());
 
@@ -24,6 +27,8 @@ class RandomWordState extends State<RandomWords> {
 
   // Current barrcode string
   String barcode = "";
+  String postTitle = "";
+  Future<Post> post;
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +38,7 @@ class RandomWordState extends State<RandomWords> {
         actions: <Widget>[
           new IconButton(
             icon: const Icon(Icons.list),
-            onPressed: _pushSaved,
+            onPressed: getPostTitle,
           ),
           new IconButton(
             icon: const Icon(Icons.camera),
@@ -42,8 +47,30 @@ class RandomWordState extends State<RandomWords> {
         ],
       ),
       //body: _buildSuggestions(),
-      body: Center(
-        child: new Text(this.barcode),
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Text(this.barcode),
+                FutureBuilder<Post>(
+                  future: fetchPost(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Text(snapshot.data.title);
+                    } else if (snapshot.hasError) {
+                      return Text("${snapshot.error}");
+                    }
+
+                    // By default, show a loading spinner
+                    return CircularProgressIndicator();
+                  },
+                )
+              ],
+            ),
+          )
+        ],
+        //child: new Text(this.barcode),
       ),
     );
   }
@@ -137,9 +164,49 @@ class RandomWordState extends State<RandomWords> {
       setState(() => this.barcode = 'Unknown error: $e');
     }
   }
+
+  Future<Post> fetchPost() async {
+    final int postId = random.randomBetween(1, 100);
+    final http.Response response =
+      await http.get("https://jsonplaceholder.typicode.com/posts/$postId");
+    if(response.statusCode == 200) {
+      return Post.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load post');
+    }
+  }
+
+  getPostTitle() async {
+    final post = await this.fetchPost();
+    print('${post.title}');
+    setState(() => this.postTitle = post.title);
+  }
+
+  setPost() {
+    final post = this.fetchPost();
+    this.post = post;
+  }
 }
 
 class RandomWords extends StatefulWidget {
   @override
   RandomWordState createState() => new RandomWordState();
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json["userId"],
+      id: json["id"],
+      title: json["title"],
+      body: json["body"]
+    );
+  }
 }
